1136fe0a1d4c5a094caa4fc25cb34c86be2d6244
Andres Ruiz <andresruizids@gmail.com>
2022-01-23 15:15:31 -0500

    base nueva actualizacion de vista general

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,32 +7,8 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    @foreach ($images as $image)
 
-                    <img src="{{route('image.item',['filename'=>$image->image_path])}}" alt="">
-                    <strong>name</strong>: {{$image->user->name}} {{$image->user->surname}} <br>
-                    <strong>comments</strong>: <br>
-                    <ul>
-                        @foreach($image->comments as $comment)
-                        <li>{{$comment->comment}}
-                            <br>
-                            <em>User: {{$comment->user->name}} {{$comment->user->surname}}</em>
-                        </li><br>
-                        @endforeach
-                    </ul>
-                    <strong>likes:</strong>{{count($image->likes)}}
-                    (
-                    @foreach($image->likes as $like)
-                    {{$like->user->name}},
-                    @endforeach
-                    )
-                    <hr>
-                    @endforeach
-                </div>
-            </div>
-        </div>
+                    @include('includes.publication', $images)
+
     </div>
 </x-app-layout>
